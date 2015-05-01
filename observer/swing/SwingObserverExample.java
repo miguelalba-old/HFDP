08d09b6a24fdbf7fd9edf0ce9ae13b4e4342c044
miguelalba89 <miguelalba89@gmail.com>
2015-05-01 19:53:23 +0200

    Swing observer example

---
+++ observer/swing/SwingObserverExample.java
@@ -0,0 +1,33 @@
+package observer.swing;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class SwingObserverExample {
+
+	JFrame frame;
+
+	public static void main(String[] args) {
+		SwingObserverExample example = new SwingObserverExample();
+		example.go();
+	}
+
+	public void go() {
+		frame = new JFrame();
+
+		JButton button = new JButton("Should I do it?");
+
+		button.addActionListener(event -> System.out
+				.println("Don't do it, you might regret i!"));
+		button.addActionListener(event -> System.out.println("Come'on, do it!"));
+		frame.getContentPane().add(BorderLayout.CENTER, button);
+		
+		// Set frame properties
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(BorderLayout.CENTER, button);
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+	}
+}
